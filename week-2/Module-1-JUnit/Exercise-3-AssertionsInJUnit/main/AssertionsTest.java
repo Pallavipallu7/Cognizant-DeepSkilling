db4db1a683b51ffbeaf2b2cdb4861ad4bbe8661b
Pallavipallu7 <145217501+Pallavipallu7@users.noreply.github.com>
2026-07-27 21:22:39 +0530

    Update AssertionsTest.java

--- week-2/Module-1-JUnit/Exercise-3-AssertionsInJUnit/main/AssertionsTest.java
+++ week-2/Module-1-JUnit/Exercise-3-AssertionsInJUnit/main/AssertionsTest.java
@@ -1,27 +1,13 @@
 import org.junit.Test;
-
 import static org.junit.Assert.*;
-
 public class AssertionsTest {
-
     @Test
     public void testAssertions() {
-
         Calculator calculator = new Calculator();
-
-        // Assert Equals
         assertEquals(5, calculator.add(2, 3));
-
-        // Assert True
         assertTrue(5 > 3);
-
-        // Assert False
         assertFalse(5 < 3);
-
-        // Assert Null
         assertNull(null);
-
-        // Assert Not Null
         assertNotNull(new Object());
     }
 }
