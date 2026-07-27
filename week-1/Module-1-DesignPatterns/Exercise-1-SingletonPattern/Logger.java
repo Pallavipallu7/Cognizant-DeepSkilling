d329d71aa4b58d88af5f9bbfe9c59cf306c557a8
Pallavipallu7 <145217501+Pallavipallu7@users.noreply.github.com>
2026-07-27 17:23:04 +0530

    Update Logger.java

--- week-1/Module-1-DesignPatterns/Exercise-1-SingletonPattern/Logger.java
+++ week-1/Module-1-DesignPatterns/Exercise-1-SingletonPattern/Logger.java
@@ -2,13 +2,9 @@ package Week1;
 
 public class Logger {
     private static Logger instance;
-
-    // Step 2: Make constructor private
     private Logger() {
         System.out.println("Logger Instance Created");
     }
-
-    // Step 3: Provide public method to access instance
     public static Logger getInstance() {
 
         if (instance == null) {
