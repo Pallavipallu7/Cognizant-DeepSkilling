c14b6dfb4d0c76299546d5d1c8b4e577865c2985
Pallavipallu7 <145217501+Pallavipallu7@users.noreply.github.com>
2026-07-28 16:45:05 +0530

    Update CountryService.java

--- week-4/Module-6-Spring-Data-JPA-Hibernate/HandsOn-5-CountryService/src/main/java/com/cognizant/ormlearn/service/CountryService.java
+++ week-4/Module-6-Spring-Data-JPA-Hibernate/HandsOn-5-CountryService/src/main/java/com/cognizant/ormlearn/service/CountryService.java
@@ -1,1 +1,43 @@
+package com.cognizant.ormlearn.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.repository.CountryRepository;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository repository;
+
+    @Transactional(readOnly = true)
+    public Country getCountry(String code) {
+        return repository.findById(code).orElse(null);
+    }
+
+    @Transactional
+    public Country addCountry(Country country) {
+        return repository.save(country);
+    }
+
+    @Transactional
+    public Country updateCountry(Country country) {
+        return repository.save(country);
+    }
+
+    @Transactional
+    public void deleteCountry(String code) {
+        repository.deleteById(code);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Country> searchCountry(String name) {
+        return repository.findByNameContainingIgnoreCase(name);
+    }
+
+}
